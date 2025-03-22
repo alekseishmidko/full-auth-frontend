87d077ab5b3a9accbd9ad23b44918321372a4c7b
alekseishmidko <alexeishmidko@gmail.com>
2025-03-22 22:19:11 +0400

    shared

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ 
+  experimental: { 
+	},
+	env: {
+		SERVER_URL: process.env.SERVER_URL,
+		GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'avatars.yandex.net'
+			}
+		]
+	}
 };
 
 export default nextConfig;
